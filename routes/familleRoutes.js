d14cea7f35825ef6d8d88dd02ca0f74204226c54
Mat95rix7 <ndjam71@gmail.com>
2025-07-28 22:55:09 +0200

    test sans auth

--- routes/familleRoutes.js
+++ routes/familleRoutes.js
@@ -6,7 +6,10 @@ const auth = require('../middlewares/auth');
 // router.use(express.json());
 // router.use(express.urlencoded({ extended: true }));
 
-router.get('/', auth, familleController.getHommesMaries);
-router.get('/:pereId', auth, familleController.getFamilleByHommeMarie);
+router.get('/', familleController.getHommesMaries);
+router.get('/:pereId', familleController.getFamilleByHommeMarie);
+
+// router.get('/', auth, familleController.getHommesMaries);
+// router.get('/:pereId', auth, familleController.getFamilleByHommeMarie);
 
 module.exports = router;
--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -5,11 +5,17 @@ const upload = require('../middlewares/upload');
 const auth = require('../middlewares/auth');
 const isAdmin = require('../middlewares/isAdmin');
 
+// router.get('/', personController.getAll);
+// router.get('/:id', auth, personController.getOne);
+// router.delete('/:id', auth, isAdmin, personController.delete);
+// router.post('/', auth, isAdmin, upload.single('photo'), personController.create);
+// router.put('/:id', auth, isAdmin, upload.single('photo'), personController.update);
+
 router.get('/', personController.getAll);
-router.get('/:id', auth, personController.getOne);
-router.delete('/:id', auth, isAdmin, personController.delete);
-router.post('/', auth, isAdmin, upload.single('photo'), personController.create);
-router.put('/:id', auth, isAdmin, upload.single('photo'), personController.update);
+router.get('/:id', personController.getOne);
+router.delete('/:id', personController.delete);
+router.post('/', upload.single('photo'), personController.create);
+router.put('/:id', upload.single('photo'), personController.update);
 
 
 // router.use(express.json());
