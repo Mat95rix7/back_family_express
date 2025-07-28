9acc8d3d4bdb3c57c8cb56c2e2f1e1fb62a225ca
Mat95rix7 <ndjam71@gmail.com>
2025-07-28 22:58:10 +0200

    remettre auth

--- routes/familleRoutes.js
+++ routes/familleRoutes.js
@@ -6,10 +6,7 @@ const auth = require('../middlewares/auth');
 // router.use(express.json());
 // router.use(express.urlencoded({ extended: true }));
 
-router.get('/', familleController.getHommesMaries);
-router.get('/:pereId', familleController.getFamilleByHommeMarie);
-
-// router.get('/', auth, familleController.getHommesMaries);
-// router.get('/:pereId', auth, familleController.getFamilleByHommeMarie);
+router.get('/', auth, familleController.getHommesMaries);
+router.get('/:pereId', auth, familleController.getFamilleByHommeMarie);
 
 module.exports = router;
